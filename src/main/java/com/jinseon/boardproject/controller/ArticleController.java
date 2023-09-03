2e098afe84fdc7724fc3b9b0532a373c8cff5c4b
JinseonYe <aatwe24@gmail.com>
2023-09-03 12:13:14 +0900

    #16 - 게시글 뷰 기본 골격 작성 컨트롤러, 뷰, 테스트까지 한 번에 적용

--- src/main/java/com/jinseon/boardproject/controller/ArticleController.java
+++ src/main/java/com/jinseon/boardproject/controller/ArticleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/articles")
@@ -14,6 +15,15 @@ public class ArticleController {
 	@GetMapping
 	public String articles(ModelMap map) {
 		map.addAttribute("articles", List.of());
+
 		return "articles/index";
+	}
+
+	@GetMapping("/{articleId}")
+	public String articles(@PathVariable Long articleId, ModelMap map) {
+		map.addAttribute("article", "article"); //TODO: 구현 시 실제 데이터로 변경 필요
+		map.addAttribute("articleComments", List.of());
+
+		return "articles/detail";
 	}
 }
